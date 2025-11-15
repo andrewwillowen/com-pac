7f954586363ff1139ddf327812a57cc747c48cf4
andrewwillowen <60939910+andrewwillowen@users.noreply.github.com>
2025-11-14 20:51:55 -0600

    Finished reducing main

--- src/com_pac/core.py
+++ src/com_pac/core.py
@@ -339,7 +339,7 @@ def get_principal_axes(isotopologue_names, isotopologue_dict, n_atoms, atom_symb
         rotational_constants[iso] = list(map(inertiaToRot, evals))
 
     return (
-        atom_masses, atom_mass_numbers, rotational_constants,
+        atom_masses, rotational_constants,
         pa_dipoles, pa_coordinates, pa_inertias,
         com_coordinates, com_inertias, eigenvectors, eigenvalues,
     )
@@ -400,84 +400,36 @@ def get_dataframes(atom_masses, atom_symbols, rotational_constants, pa_dipoles, 
     )
 
 
-def main():
-    input_file_path, num_of_decimals = read_args(6)
-
-    # ================================ #
-    #  reading contents of input file  #
-    # ================================ #
-
-    if input_file_path is not None:
-        input_file_dir = input_file_path.parent
-        input_file_name = input_file_path.name
-    else:
-        raise ValueError("Failure to import file path.")
-
-    with open(input_file_path, "r") as infile:
-        input_file = infile.read()
-
-    (
-        isotopologue_names, isotopologue_dict, n_atoms,
-        atom_symbols, mol_coordinates, mol_dipole,
-        atom_numbering
-    ) = parse_input_file(input_file)
+def header_creator(some_text: str):
+    if not isinstance(some_text, str):
+        try:
+            some_text = str(some_text)
+        except (Exception,):
+            raise TypeError("TypeError: Bad input passed to header_creator")
+    str_len = len(some_text)
+    border_len = str_len + 2
+    border_str = "# {} #".format("=" * border_len)
+    header_str = "#  {}  #".format(some_text)
+    out_str = "{}".format("\n".join([border_str, header_str, border_str]))
+    return out_str
 
 
-    (
-        atom_masses, atom_mass_numbers, rotational_constants,
-        pa_dipoles, pa_coordinates, pa_inertias,
-        com_coordinates, com_inertias, eigenvectors, eigenvalues,
-    ) = get_principal_axes(isotopologue_names, isotopologue_dict, n_atoms, atom_symbols, mol_coordinates, mol_dipole)
+def df_text_export(dataframe: pd.DataFrame, n_decimals=6):
+    def do_format(some_number):
+        nice_number = "{:.{n}f}".format(some_number, n=n_decimals)
+        return nice_number
 
-    (
-        atom_masses_df, rotational_constants_df, dipole_components_df, com_coordinates_df_dict,
-        com_inertias_df_dict, eigenvectors_df_dict, pa_inertias_df_dict, pa_coordinates_df_dict,
-    ) = get_dataframes(atom_masses, atom_symbols, rotational_constants, pa_dipoles, isotopologue_names, com_coordinates, atom_numbering,
-                   com_inertias, eigenvectors, pa_inertias, pa_coordinates)
-
-    # ==================== #
-    #  Outputting results  #
-    # ==================== #
+    return dataframe.applymap(do_format).to_string()
 
 
-    def header_creator(some_text: str):
-        if not isinstance(some_text, str):
-            try:
-                some_text = str(some_text)
-            except (Exception,):
-                raise TypeError("TypeError: Bad input passed to header_creator")
-        str_len = len(some_text)
-        border_len = str_len + 2
-        border_str = "# {} #".format("=" * border_len)
-        header_str = "#  {}  #".format(some_text)
-        out_str = "{}".format("\n".join([border_str, header_str, border_str]))
-        return out_str
-
-
-    if input_file_name.count(".") != 1:
-        input_file_base_name = str(input_file_name)
-    else:
-        input_file_base_name = str(input_file_name).split(".")[0]
-
-    text_output_name = input_file_base_name + "_pac.out"
-    csv_output_name = input_file_base_name + "_pac.csv"
-
-    text_output_path = input_file_dir.joinpath(text_output_name)
-    csv_output_path = input_file_dir.joinpath(csv_output_name)
-
-
+def generate_output_file(num_of_decimals, csv_output_name, input_file, atom_masses_df, rotational_constants_df,
+                         dipole_components_df, isotopologue_names, com_coordinates_df_dict, atom_symbols,
+                         com_inertias_df_dict, eigenvectors_df_dict, eigenvalues, pa_inertias_df_dict,
+                         pa_coordinates_df_dict, text_output_path):
     # TEXT OUTPUT
     #
     # All numbers are "friendly", that is, not in scientific notation.
     # Full numbers are provided in the .csv output.
-
-
-    def df_text_export(dataframe: pd.DataFrame, n_decimals=num_of_decimals):
-        def do_format(some_number):
-            nice_number = "{:.{n}f}".format(some_number, n=n_decimals)
-            return nice_number
-
-        return dataframe.applymap(do_format).to_string()
 
 
     preamble = """The numbers in this output have been limited to {} decimal places.
@@ -489,7 +441,7 @@ def main():
 
     input_section = [header_creator("Raw Input"), input_file.strip()]
 
-    nice_atomic_masses = df_text_export(atom_masses_df)
+    nice_atomic_masses = df_text_export(atom_masses_df, n_decimals=num_of_decimals)
     am_width = max([len(i) for i in nice_atomic_masses.split("\n")])
     nice_atomic_masses = nice_atomic_masses.replace(
         "\nTotal", "\n{}\nTotal".format("-" * am_width)
@@ -498,12 +450,12 @@ def main():
 
     rotational_constants_section = [
         header_creator("Rotational Constants"),
-        df_text_export(rotational_constants_df),
+        df_text_export(rotational_constants_df, n_decimals=num_of_decimals),
     ]
 
     dipole_components_section = [
         header_creator("Dipole Components"),
-        df_text_export(dipole_components_df),
+        df_text_export(dipole_components_df, n_decimals=num_of_decimals),
     ]
 
     iso_com_coordinates_entries = []
@@ -515,14 +467,14 @@ def main():
         iso_com_df = com_coordinates_df_dict[iso].copy(deep=True)
         iso_com_df.index = atom_symbols
         iso_com_coordinate = "{}\n{}".format(
-            iso, df_text_export(com_coordinates_df_dict[iso])
+            iso, df_text_export(com_coordinates_df_dict[iso], n_decimals=num_of_decimals)
         )
         iso_com_coordinates_entries.append(iso_com_coordinate)
 
-        iso_com_inertia = "{}\n{}".format(iso, df_text_export(com_inertias_df_dict[iso]))
+        iso_com_inertia = "{}\n{}".format(iso, df_text_export(com_inertias_df_dict[iso], n_decimals=num_of_decimals))
         iso_com_inertias_entries.append(iso_com_inertia)
 
-        iso_eigen_vec = df_text_export(eigenvectors_df_dict[iso])
+        iso_eigen_vec = df_text_export(eigenvectors_df_dict[iso], n_decimals=num_of_decimals)
         formatted_eigen_val = []
         for eigen_val in eigenvalues[iso]:
             formatted_eigen_val.append("{:.{n}}".format(eigen_val, n=num_of_decimals))
@@ -532,14 +484,14 @@ def main():
         )
         iso_eigens_entries.append(iso_eigen)
 
-        iso_pa_inertia = "{}\n{}".format(iso, df_text_export(pa_inertias_df_dict[iso]))
+        iso_pa_inertia = "{}\n{}".format(iso, df_text_export(pa_inertias_df_dict[iso], n_decimals=num_of_decimals))
         iso_pa_inertias_entries.append(iso_pa_inertia)
 
         iso_pa_df = pa_coordinates_df_dict[iso].copy(deep=True)
         iso_pa_df.index = atom_symbols
         iso_pa_df.loc["Dipole"] = list(dipole_components_df[iso])
         iso_pa_df.loc["Rot. Con."] = list(rotational_constants_df[iso])
-        iso_result = "{}\n{}".format(iso, df_text_export(iso_pa_df))
+        iso_result = "{}\n{}".format(iso, df_text_export(iso_pa_df, n_decimals=num_of_decimals))
         width = max([len(i) for i in iso_result.split("\n")])
         iso_result = iso_result.replace("\nDip", "\n{}\nDip".format("-" * width))
         iso_results_entries.append(iso_result)
@@ -593,7 +545,7 @@ def main():
     with open(text_output_path, "w") as outfile:
         outfile.write(file_string)
 
-
+def generate_csv_output(pa_coordinates_df_dict, rotational_constants_df, dipole_components_df, atom_masses_df, csv_output_path):
     # .csv file
     # Outputs all data without formatting; scientific notation may be used in the values.
 
@@ -614,6 +566,65 @@ def main():
 
     with open(csv_output_path, "w") as outfile:
         outfile.write(csv_file_string)
+
+def main():
+    input_file_path, num_of_decimals = read_args(6)
+
+    # ================================ #
+    #  reading contents of input file  #
+    # ================================ #
+
+    if input_file_path is not None:
+        input_file_dir = input_file_path.parent
+        input_file_name = input_file_path.name
+    else:
+        raise ValueError("Failure to import file path.")
+
+    with open(input_file_path, "r") as infile:
+        input_file = infile.read()
+
+    (
+        isotopologue_names, isotopologue_dict, n_atoms,
+        atom_symbols, mol_coordinates, mol_dipole,
+        atom_numbering
+    ) = parse_input_file(input_file)
+
+
+    (
+        atom_masses, rotational_constants,
+        pa_dipoles, pa_coordinates, pa_inertias,
+        com_coordinates, com_inertias, eigenvectors, eigenvalues,
+    ) = get_principal_axes(isotopologue_names, isotopologue_dict, n_atoms, atom_symbols, mol_coordinates, mol_dipole)
+
+    (
+        atom_masses_df, rotational_constants_df, dipole_components_df, com_coordinates_df_dict,
+        com_inertias_df_dict, eigenvectors_df_dict, pa_inertias_df_dict, pa_coordinates_df_dict,
+    ) = get_dataframes(atom_masses, atom_symbols, rotational_constants, pa_dipoles, isotopologue_names, com_coordinates, atom_numbering,
+                   com_inertias, eigenvectors, pa_inertias, pa_coordinates)
+
+    # ==================== #
+    #  Outputting results  #
+    # ==================== #
+
+
+    if input_file_name.count(".") != 1:
+        input_file_base_name = str(input_file_name)
+    else:
+        input_file_base_name = str(input_file_name).split(".")[0]
+
+    text_output_name = input_file_base_name + "_pac.out"
+    csv_output_name = input_file_base_name + "_pac.csv"
+
+    text_output_path = input_file_dir.joinpath(text_output_name)
+    csv_output_path = input_file_dir.joinpath(csv_output_name)
+
+
+    generate_output_file(num_of_decimals, csv_output_name, input_file, atom_masses_df, rotational_constants_df,
+                         dipole_components_df, isotopologue_names, com_coordinates_df_dict, atom_symbols,
+                         com_inertias_df_dict, eigenvectors_df_dict, eigenvalues, pa_inertias_df_dict,
+                         pa_coordinates_df_dict, text_output_path)
+
+    generate_csv_output(pa_coordinates_df_dict, rotational_constants_df, dipole_components_df, atom_masses_df, csv_output_path)
 
 if __name__ == "__main__":
     main()
